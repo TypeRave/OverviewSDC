d8882621e04b41f2d2e5ed1cf5471e8e62459600
Taryn <TarynCovert89@gmail.com>
2023-09-06 10:29:56 -0700

    comeon

---
+++ Server/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const controllers = require('./controllers');
+
+const app = express();
+
+app.use(express.json());
+app.get('/product', controllers.getProduct);
+app.get('/product/features', controllers.getFeatures);
+app.get('/product/styles', controllers.getStyles);
+app.get('/product/related', controllers.getRelated);
+app.get('/product/relatedStyle', controllers.getStyles);
+
+const PORTSERV = process.env.PORTSERV || 3000;
+app.listen(PORTSERV, () => {
+  console.log(`Server is listening on port ${PORTSERV}`);
+});
