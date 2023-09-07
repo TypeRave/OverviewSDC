88260b65dcaeb629b00dc915c258ff865ab8eaae
Taryn <TarynCovert89@gmail.com>
2023-09-06 22:46:24 -0700

    Update to INdex

--- Server/database/index.js
+++ Server/database/index.js
@@ -1,12 +1,20 @@
 require('dotenv').config();
 const { Client } = require('pg');
 
+// const client = new Client({
+//   host: 'localhost',
+//   user: process.env.USER,
+//   port: process.env.PORT,
+//   password: 'postgres',
+//   database: 'productdb',
+// });
+
 const client = new Client({
-  host: 'localhost',
+  host: process.env.HOST,
   user: process.env.USER,
   port: process.env.PORT,
-  password: 'postgres',
-  database: 'productdb',
+  password: process.env.PASS,
+  database: process.env.DATABASE,
 });
 
 client.connect()
